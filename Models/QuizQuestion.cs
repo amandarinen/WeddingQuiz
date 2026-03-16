4ec995120668b8cadd145477052e9df3296187eb
Amanda <amanda@leijonborg.se>
2026-03-16 15:48:31 +0100

    Initial commit Wedding Quiz

---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,10 @@
+﻿namespace WeddingQuiz.Models
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; } = "";
+        public List<string> Answers { get; set; } = new();
+        public string CorrectAnswer { get; set; } = "";
+    }
+
+}
