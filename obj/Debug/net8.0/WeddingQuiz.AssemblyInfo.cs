edeb63038ca133006d75875580741f79fa0b2649
Amanda <amanda@leijonborg.se>
2026-03-16 20:14:24 +0100

    Navigation on Home now works

--- obj/Debug/net8.0/WeddingQuiz.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeddingQuiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeddingQuiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c0b0cc9583efc722f13c7708b6abe07fa1e6b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b03553347462a7b2af68d74dc5c5a874099366b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeddingQuiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeddingQuiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
